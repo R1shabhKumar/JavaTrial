35ea226cf461bf980fd5d8e2f805851d963ba8df
R1shabhKumar <hs3189028@gmail.com>
2024-08-31 04:17:08 +0530

    First draft of the Guesser Game

---
+++ Guessergame.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+class Guesser{
+    int guessNum;
+    public int guessingNumber(){
+        System.out.println("Guesser! Kindly guess the number...");
+        Scanner sc = new Scanner(System.in);
+        guessNum = sc.nextInt();
+        return guessNum;
+    }
+}
+class Player{
+    int guessNum;
+    int guessingNumber(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Player! kindly guess your number");
+        guessNum = sc.nextInt();
+        return guessNum;
+    }
+}
+class Umpire{
+    int numFromGuesser;
+    int numFromPlayer1;
+    int numFromPlayer2;
+    int numFromPlayer3;
+
+    void collectFromGuesser(){
+        Guesser g = new Guesser();
+        numFromGuesser=g.guessingNumber();
+    }
+    void collectFromPlayer(){
+        Player p1 = new Player();
+        Player p2 = new Player();
+        Player p3 = new Player();
+        numFromPlayer1 = p1.guessingNumber();
+        numFromPlayer2 = p2.guessingNumber();
+        numFromPlayer3 = p3.guessingNumber();
+    }
+    void compare(){
+        if(numFromGuesser==numFromPlayer1){
+            if(numFromGuesser==numFromPlayer2 && numFromGuesser==numFromPlayer3){
+            System.out.println("Everybody won");
+            }
+            else if(numFromGuesser==numFromPlayer2){
+                System.out.println("Player 1 and Player 2 both won the game");
+            }
+            else if(numFromGuesser==numFromPlayer3){
+                System.out.println("Player1 and Player 3 both won the game");
+            }else{
+                System.out.println("Player 1 won the game");
+            }
+            
+        }
+        else if(numFromGuesser==numFromPlayer2){
+            if(numFromGuesser==numFromPlayer3){
+                System.out.println("Player 2 and Player 3 both won the game");
+            }else{
+                System.out.println("Player 2 won the game");
+            }
+            
+        }
+        else if(numFromGuesser==numFromPlayer3){
+            System.out.println("Player 3 won the game");
+        }
+        else{
+            System.out.println("Nobody won!!");
+        }
+    }
+}
+public class Guessergame {
+    public static void main(String[] args) {
+        System.out.println("Game started");
+        Umpire u = new Umpire();
+        u.collectFromGuesser();
+        u.collectFromPlayer();
+        u.compare();
+    }
+}
+
