8f4536e600dd837dcaf1dc60ec182c5b43cc7575
R1shabhKumar <hs3189028@gmail.com>
2024-08-30 05:43:12 +0530

    More comment added

--- pattern.java
+++ pattern.java
@@ -5,6 +5,7 @@ public class pattern {
         System.out.println();
         System.out.println();
         for(int i=0;i<n;i++){
+            //for printing "R"
             for(int j=0;j<n;j++){
                 if(i==0||i==(n-1)/2||j==0||j==(n-1)&&i<=(n-1)/2||i-j==(n+1)/2){
                 System.out.print("*");
@@ -12,7 +13,8 @@ public class pattern {
                     System.out.print(" ");
                 }
             }
-            System.out.print(" ");
+            System.out.print(" ");//Used for space between the figures
+            //for printing "I"
             for(int j=0;j<n;j++){
                 if(j==(n+1)/2){
                 System.out.print("*");
@@ -21,6 +23,7 @@ public class pattern {
                 }
             }
             System.out.print(" ");
+            //for printing "S"
             for(int j=0;j<n;j++){
                 if(i==0||j==0&&i<=(n+1)/2||i==(n+1)/2||i==n-1||j==n-1&&i>=(n+1)/2){
                 System.out.print("*");
@@ -30,6 +33,7 @@ public class pattern {
             }
             System.out.print(" ");
             System.out.print(" ");
+            //for printing "H"
             for(int j=0;j<n;j++){
                 if(i==(n+1)/2||j==0||j==n-1){
                 System.out.print("*");
@@ -39,6 +43,7 @@ public class pattern {
             }
             System.out.print(" ");
             System.out.print(" ");
+            //for printing "A"
             for(int j=0;j<n;j++){
                 if(i==0||i==(n+1)/2||j==0||j==n-1){
                 System.out.print("*");
@@ -48,6 +53,7 @@ public class pattern {
             }
             System.out.print(" ");
             System.out.print(" ");
+            //for printing "B"
             for(int j=0;j<n;j++){
                 if(i==0||i==(n+1)/2||i==n-1||j==0||j==n-1){
                 System.out.print("*");
@@ -57,6 +63,7 @@ public class pattern {
             }
             System.out.print(" ");
             System.out.print(" ");
+            //for printing "H" again
             for(int j=0;j<n;j++){
                 if(i==(n+1)/2||j==0||j==n-1){
                 System.out.print("*");
