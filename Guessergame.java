1166d3b2d1ce1976e23b15ce21fbaa7fda8fb055
R1shabhKumar <hs3189028@gmail.com>
2024-08-31 05:36:39 +0530

    Modified the game into a 3 round format

--- Guessergame.java
+++ Guessergame.java
@@ -20,6 +20,9 @@ class Umpire{
     int numFromPlayer1;
     int numFromPlayer2;
     int numFromPlayer3;
+    int score1;
+    int score2;
+    int score3;
 
     void collectFromGuesser(){
         Guesser g = new Guesser();
@@ -33,32 +36,97 @@ class Umpire{
         numFromPlayer2 = p2.guessingNumber();
         numFromPlayer3 = p3.guessingNumber();
     }
+    void score(){
+        if(score1==score2&&score1==score3&&score1!=0){
+            System.out.println("Match tied at score "+score1+" nobody won!!!");
+        }else if(score1>score2){
+            if(score1>score3){
+                System.out.println("Player 1 won by scoring "+score1);
+            }else if(score1==score3){
+                System.out.println("Player 1 and Player 3 both won by scoring "+score1);
+            }else{
+                System.out.println("Player 3 won by scoring "+score3);
+            }
+        }else if(score1>score3){
+            if(score1>score2){
+                System.out.println("Player 1 won by scoring "+score1);
+            }else if(score1==score2){
+                System.out.println("Player 1 and Player 2 both won by scoring "+score1);
+            }else{
+                System.out.println("Player 2 won by scoring "+score2);
+            }
+        }else if(score1==score2){
+            if(score1>score3){
+                System.out.println("Player 1 and Player 3 won by scoring "+score1);
+            }else{
+                System.out.println("Player 3 won by scoring "+score3);
+            }
+        }else if(score1==score3){
+            if(score1>score2){
+                System.out.println("Player 1 and Player 3 won by scoring "+score1);
+            }else{
+                System.out.println("Player 2 won by scoring "+score2);
+            }
+        }else if(score2==score3){
+            if(score2>score1){
+                System.out.println("Player 2 and Player 3 won by scoring "+score2);
+            }else{
+                System.out.println("Player 1 won by scoring "+score1);
+            }
+        }else if(score2>score3){
+            if(score2>score1){
+                System.out.println("Player 2 won by scoring "+score2);
+            }else{
+                System.out.println("Player1 won by scoring "+score1);
+            }
+        }else if(score3>score1){
+            if(score3>score2){
+                System.out.println("Player 3 won by scoring "+score3);
+            }else{
+                System.out.println("Player 2 won by scoring "+score2);
+            }
+        }else{
+            System.out.println("Nobody won!! Everyone scored 0");
+        }
+    }
     void compare(){
         
         if(numFromGuesser==numFromPlayer1){
             if(numFromGuesser==numFromPlayer2 && numFromGuesser==numFromPlayer3){
-            System.out.println("Everybody won by guesssing the number "+numFromGuesser);
+            System.out.println("Everybody won this round by guesssing the number "+numFromGuesser);
+            score1++;
+            score2++;
+            score3++;
             }
             else if(numFromGuesser==numFromPlayer2){
-                System.out.println("Player 1 and Player 2 both won the game by guessing the number "+numFromGuesser);
+                System.out.println("Player 1 and Player 2 both won the round by guessing the number "+numFromGuesser);
+                score1++;
+                score2++;
             }
             else if(numFromGuesser==numFromPlayer3){
-                System.out.println("Player1 and Player 3 both won the game by guessing the number "+numFromGuesser);
+                System.out.println("Player1 and Player 3 both won the round by guessing the number "+numFromGuesser);
+                score1++;
+                score3++;
             }else{
-                System.out.println("Player 1 won the game by guessing the number "+numFromGuesser);
+                System.out.println("Player 1 won the round by guessing the number "+numFromGuesser);
+                score1++;
             }
             
         }
         else if(numFromGuesser==numFromPlayer2){
             if(numFromGuesser==numFromPlayer3){
-                System.out.println("Player 2 and Player 3 both won the game by guessing the number "+numFromGuesser);
+                System.out.println("Player 2 and Player 3 both won the round by guessing the number "+numFromGuesser);
+                score2++;
+                score3++;
             }else{
-                System.out.println("Player 2 won the game by guessing the number "+numFromGuesser);
+                System.out.println("Player 2 won the round by guessing the number "+numFromGuesser);
+                score2++;
             }
             
         }
         else if(numFromGuesser==numFromPlayer3){
-            System.out.println("Player 3 won the game by guessing the number "+numFromGuesser);
+            System.out.println("Player 3 won the round by guessing the number "+numFromGuesser);
+            score3++;
         }
         else{
             System.out.println("Nobody won!! the right answer is "+numFromGuesser);
@@ -67,11 +135,17 @@ class Umpire{
 }
 public class Guessergame {
     public static void main(String[] args) {
-        System.out.println("Game started");
         Umpire u = new Umpire();
-        u.collectFromGuesser();
-        u.collectFromPlayer();
-        u.compare();
+        for(int i=0;i<3;i++){
+            System.out.println("Game started");
+            System.out.println("Round "+(i+1));
+            
+            u.collectFromGuesser();
+            u.collectFromPlayer();
+            u.compare();
+            System.out.println();
+        }
+    u.score();    
     }
 }
 
